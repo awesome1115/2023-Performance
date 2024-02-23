b3d88c8b18e6189cf15e4d875c0ad2069ec37b55
awesome1115 <arvidjohansson413@gmail.com>
2024-02-24 00:03:19 +0900

    add-string

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -16,7 +16,7 @@ export default async function Page() {
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Dashboard
+        Dashboard Page
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Card title="Collected" value={totalPaidInvoices} type="collected" />
